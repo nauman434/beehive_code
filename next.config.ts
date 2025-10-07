ee68178e1101f8b8a22181239b00354a53a46f9c
Nauman <ijaznauman68@gmail.com>
2025-10-07 01:01:13 +0100

    rebuild the modules

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
   },
+  experimental: {
+    esmExternals: 'loose',
+  },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
